9c58f26e57d24ecad86747c59e2649612f18190f
abhishek-singh0 <abhishek.programs@gmail.com>
2021-07-07 19:16:41 +0530

    "Title"

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,13 @@
+from django.core.checks import messages
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class contact(models.Model):
+    name = models.CharField(max_length=50)
+    messages = models.CharField(max_length=120)
