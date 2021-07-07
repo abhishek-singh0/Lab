8e36d632413e352f4865b5bb9f8fd3bac3bee5a6
abhishek-singh0 <abhishek.programs@gmail.com>
2021-07-07 19:08:21 +0530

    "Views"

--- Home/views.py
+++ Home/views.py
@@ -9,3 +9,12 @@ def index(request):
 
 def login(request):
     return render(request, 'home/login.html')
+
+def register(request):
+    return render(request, 'home/register.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
